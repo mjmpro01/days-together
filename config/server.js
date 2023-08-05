941fe9bf715c52280e17c992e7925185e6f2d612
minhle <duyminh2032000@gmail.com>
2023-08-06 00:17:56 +0700

    heelo2

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '54.151.157.125'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'http://mjadev.xyz'),
+  url: env('PUBLIC_URL', 'http://mjadev.xyz:1337'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
