a3cf3144b0f0b550f3db5a446833f5425e63cb45
minhle <duyminh2032000@gmail.com>
2023-08-07 14:12:06 +0700

    fix upload s3

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,38 +1,42 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-    {
-      name: 'strapi::security',
-      config: {
-        contentSecurityPolicy: {
-          useDefaults: true,
-          directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': [
-              "'self'",
-              'data:',
-              'blob:',
-              'market-assets.strapi.io',
-              'clouds3ofmjm.s3.ap-southeast-1.amazonaws.com',
-            ],
-            'media-src': [
-              "'self'",
-              'data:',
-              'blob:',
-              'market-assets.strapi.io',
-              'clouds3ofmjm.s3.ap-southeast-1.amazonaws.com',
-            ],
-            upgradeInsecureRequests: null,
+    // ~/strapi-aws-s3/backend/config/middlewares.js
+
+    module.exports = [
+      'strapi::errors',
+      /* Replace 'strapi::security', with this snippet */
+      /* Beginning of snippet */
+      {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'strapi-days-together.s3.ap-southeast-1.amazonaws.com',
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'dl.airtable.com',
+                'strapi-days-together.s3.ap-southeast-1.amazonaws.com',
+              ],
+              upgradeInsecureRequests: null,
+            },
           },
         },
       },
-    },
-];
+      /* End of snippet */
+      'strapi::cors',
+      'strapi::poweredBy',
+      'strapi::logger',
+      'strapi::query',
+      'strapi::body',
+      'strapi::session',
+      'strapi::favicon',
+      'strapi::public',
+    ];
